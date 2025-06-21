b6ca19874cb894a7209738dbc05e41597cd570d3
Kagisol <kagisoleso@yourresearch.com>
2025-06-21 17:34:46 +0200

    Understanding Angular @for tracking functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,4 +20,15 @@ ngrxCourse = COURSES[2];
   onCourseSelected(course: Course) {
     console.log('Course selected... App ts:: ', course);
   }
+
+  // In special cases where we don't have any unique identifier for our @for tracking
+  // That is when you will need to create one
+  // In html instead of using 'course.id' -> 'courseTracker'
+  // It is mandatory with the new @for -> to optimally only change parts that need to change
+  //   instead of re-rendering the whole list of the array - it has performance benefit.
+  // Not ideally recommended but to avoid compilation error you can track using the $index
+  // 'track $index'
+  courseTracker(index: number, course: Course) {
+    return course.id;
+  }
 }
